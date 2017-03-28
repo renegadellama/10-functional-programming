d00cd347dbfb55084a1bdab604c5bc3c8b46d3c8
Chris Closser <chrisclosser1@gmail.com>
2017-03-27 17:57:50 -0700

    most TODOs done

--- starter-code/server.js
+++ starter-code/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://postgres:1234@HOST:3000'; // TODO: Don't forget to set your own conString
+const conString = process.env.DATABASE_URL || 'postgres://chris:test@localhost:5432/kilovolt'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
